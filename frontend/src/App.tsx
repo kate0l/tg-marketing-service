f1a78590f1e073a178312a978f490d5ab603aed0
Egor Kurilko <egor.kurilko@gmail.com>
2025-06-21 00:34:49 +0300

    feat(header): add icon, add tooltip, add profile closure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-// import { renderRoutes } from './routes';
+import { renderRoutes } from './routes';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Layout>
         <Routes>
+          {renderRoutes()}
         </Routes>
       </Layout>
     </Router>
--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './Header';
-import Navigation from './Navigation';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       <Header />
-      <Navigation />
       <main>{children}</main>
     </div>
   );
