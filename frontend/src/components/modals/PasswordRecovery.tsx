578bae088d7a0db6fea5490e7d3945a9d574ffc9
evgenii <vovk.evgenii.199811@gmail.com>
2025-07-14 16:26:07 +0300

    change PasswordRecovery

--- frontend/src/components/modals/PasswordRecovery.tsx
+++ frontend/src/components/modals/PasswordRecovery.tsx
@@ -4,6 +4,9 @@ import "./style.css";
 const PasswordRecovery: React.FC = () => {
   return (
     <div className="password-recovery">
+      <button className="password-recovery__close">
+
+      </button>
       <h2 className="password-recovery__title">
         Восстановление пароля
       </h2>
