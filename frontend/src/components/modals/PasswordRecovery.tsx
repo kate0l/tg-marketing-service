b91dcec899dc6019c6b2545bb999e8e1437ee60c
evgenii <vovk.evgenii.199811@gmail.com>
2025-07-15 15:15:27 +0300

    add branch modal

--- frontend/src/components/modals/PasswordRecovery.tsx
+++ frontend/src/components/modals/PasswordRecovery.tsx
@@ -1,26 +1,33 @@
 import React from "react";
-import "./style.css";
 
-const PasswordRecovery: React.FC = () => {
+const PasswordRecovery: React.FC = ({ isVisible, onClose }) => {
+  if (!isVisible) return null;
+
   return (
-    <div className="password-recovery">
-      <button className="password-recovery__close">
-
-      </button>
-      <h2 className="password-recovery__title">
-        Восстановление пароля
-      </h2>
-      <label className="password-recovery__label">
-        Введите ваш email
-        <input type="email" placeholder="E-mail" className="password-recovery__input" />
-      </label>
-      <div className="password-recovery__actions">
-        <button className="password-recovery__submit">
-          Отправить ссылку
-        </button>
-        <button className="password-recovery__cancel">
-          Отмена
-        </button>
+    <div className="fixed inset-0 bg-neutral-800 flex items-center justify-center z-50">
+      <div className="rounded-lg w-xs p-3 bg-white">
+        <div className="flex justify-end">
+          <button className="!border-none !p-0 !bg-amber-500 !leading-none" onClick={() => onClose()}>
+            &times;
+          </button>
+        </div>
+        <form className="">
+          <h2 className="text-2xl mb-1">
+            Восстановление пароля
+          </h2>
+          <label className="text-sm flex flex-col gap-2.5 mb-4">
+            Введите ваш email
+            <input type="email" placeholder="E-mail" className="border-1 rounded-sm w-full pl-4 pt-2 pb-2" />
+          </label>
+          <div className="flex justify-between">
+            <button className="!bg-blue-600 text-white">
+              Отправить ссылку
+            </button>
+            <button className="!bg-white" onClick={() => onClose()}>
+              Отмена
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PasswordRecovery from '../modals/PasswordRecovery';
 
 const Home: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
       <h1>Главная страница</h1>
-      <PasswordRecovery />
+      <button
+        style={{
+          background: 'blue', color: 'white'
+        }}
+        onClick={() => { setShowModal(true) }}
+      >
+        Forgot password
+      </button>
+      <PasswordRecovery isVisible={showModal} onClose={() => setShowModal(false)} />
     </div>
   );
 };
