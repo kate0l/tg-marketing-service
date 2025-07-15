c85a718146f773d209dbb6891a9a101f1e57af5f
evgenii <vovk.evgenii.199811@gmail.com>
2025-07-15 16:52:35 +0300

    change modal PasswordRecovery

--- frontend/src/components/modals/PasswordRecovery.tsx
+++ frontend/src/components/modals/PasswordRecovery.tsx
@@ -1,34 +1,36 @@
-import React from "react";
+interface PasswordRecoveryProps {
+  isVisible: boolean;
+  onClose: () => void;
+}
 
-const PasswordRecovery: React.FC = ({ isVisible, onClose }) => {
+const PasswordRecovery: React.FC<PasswordRecoveryProps> = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
 
+  const handleBackdropClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-neutral-800 flex items-center justify-center z-50">
-      <div className="rounded-lg w-xs p-3 bg-white">
-        <div className="flex justify-end">
-          <button className="!border-none !p-0 !bg-amber-500 !leading-none" onClick={() => onClose()}>
-            &times;
+    <div className="fixed inset-0 bg-neutral-800 flex items-center justify-center z-50" onClick={handleBackdropClick}>
+      <form className="rounded-lg w-xs p-3 bg-white">
+        <h2 className="text-2xl mb-1">
+          Восстановление пароля
+        </h2>
+        <label className="text-sm flex flex-col gap-2.5 mb-4">
+          Введите ваш email
+          <input type="email" placeholder="E-mail" className="border-1 rounded-sm w-full pl-4 pt-2 pb-2" />
+        </label>
+        <div className="flex justify-between">
+          <button className="!bg-blue-600 text-white">
+            Отправить ссылку
+          </button>
+          <button className="" onClick={() => onClose()}>
+            Отмена
           </button>
         </div>
-        <form className="">
-          <h2 className="text-2xl mb-1">
-            Восстановление пароля
-          </h2>
-          <label className="text-sm flex flex-col gap-2.5 mb-4">
-            Введите ваш email
-            <input type="email" placeholder="E-mail" className="border-1 rounded-sm w-full pl-4 pt-2 pb-2" />
-          </label>
-          <div className="flex justify-between">
-            <button className="!bg-blue-600 text-white">
-              Отправить ссылку
-            </button>
-            <button className="!bg-white" onClick={() => onClose()}>
-              Отмена
-            </button>
-          </div>
-        </form>
-      </div>
+      </form>
     </div>
   )
 }
--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,21 +1,37 @@
-import { React, useState } from 'react';
+import { useState } from 'react';
+//-->
 import PasswordRecovery from '../modals/PasswordRecovery';
+//<--
 
 const Home: React.FC = () => {
+  //-->
   const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+  //<--
 
   return (
     <div>
       <h1>Главная страница</h1>
+
+      {/* --> */}
       <button
         style={{
           background: 'blue', color: 'white'
         }}
-        onClick={() => { setShowModal(true) }}
+        onClick={openModal}
       >
         Forgot password
       </button>
-      <PasswordRecovery isVisible={showModal} onClose={() => setShowModal(false)} />
+      <PasswordRecovery isVisible={showModal} onClose={closeModal} />
+      {/* <-- */}
+
     </div>
   );
 };
