7543996495dad30aada6b2161c7bc72c93747b97
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-07-29 23:44:55 +0500

    add urls fixtures

--- config/users/tests.py
+++ config/users/tests.py
@@ -1,2 +1,11 @@
+from django.test import TestCase
+from config.users.models import User
+from tests.fixtures import URL
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def setUp(self):
+        # also need to check for superuser (idk what it is tho)
+        self.user = User.objects.create_user(
+            # check if it is a valid url in form test, not here
+            avatar_image=
+        )
