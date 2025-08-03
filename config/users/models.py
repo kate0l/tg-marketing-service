d8ec4d4d6057f86a8bdb3a31991d86cbafe4f02d
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-08-03 16:22:53 +0500

    add random str for users model

--- config/users/models.py
+++ config/users/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+ROLE_MAXLENGTH = 150
+BIO_MAXLENGTH = 200
 
 # Create your models here.
 class User(AbstractUser):
     avatar_image = models.CharField(verbose_name='url изображения профиля', blank=True, null=True)
     role = models.CharField(verbose_name='роль',
-                            max_length=150)
-    bio = models.CharField(max_length=200, verbose_name='о себе', blank=True)
+                            max_length=ROLE_MAXLENGTH)
+    bio = models.CharField(max_length=BIO_MAXLENGTH, verbose_name='о себе', blank=True)
     email = models.EmailField(verbose_name="email адрес", blank=True, unique=True)
     class Meta:
         db_table = 'users'
--- config/users/tests.py
+++ config/users/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from config.users.models import User
-from tests.fixtures import URL
+from django.db import models
+from config.users.models import ROLE_MAXLENGTH, BIO_MAXLENGTH
+from tests.fixtures import urls
+from random import choices
+from string import ascii_letters, digits
+from sys import maxunicode
+
+def make_str(l: int) -> str:
+    return ''.join(choices(range(maxunicode), l))
 
 class UserModelTest(TestCase):
     def setUp(self):
         # also need to check for superuser (idk what it is tho)
         self.user = User.objects.create_user(
             # check if it is a valid url in form test, not here
-            avatar_image=
+            avatar_image=urls['valid'][0],
+            role=make_str(ROLE_MAXLENGTH),
+            bio=make_str(BIO_MAXLENGTH)
         )
