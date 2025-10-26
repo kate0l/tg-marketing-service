4879623daa0d0d70127bf645bd06c53e65ebe683
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2025-10-26 14:50:44 +0300

    add structure of main page, demo-screenshots

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react';
+import ScreenshotsDemo from '../ui/ScreenshotsDemo';
 
 const Home: React.FC = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-      <h1 className='py-5 text-4xl font-bold'>Главная страница</h1>
+    <div className="w-full bg-blue-50 items-center justify-center">
+      <div className="w-full h-150 bg-linear-to-t from-white to-blue-50 flex justify-center">
+        <div className="max-w-xs md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto py-20 px-5">
+          <div className="h-100 w-full flex gap-5 pb-10">
+            <div>
+              <h1 className="py-5 text-4xl font-bold">
+                Аналитика Telegram + тренды +
+                <span className="text-blue-500"> ИИ-помощник</span>
+              </h1>
+            </div>
+            <div className="h-full w-200 bg-gray-100 border-2">
+              TG Analytics Dashboard
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full bg-gray-50">
+        <div className="max-w-xs md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto py-20 px-5">
+          <ScreenshotsDemo />
+        </div>
+      </div>
     </div>
   );
 };
